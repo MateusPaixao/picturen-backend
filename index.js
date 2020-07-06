9eaaf25181494d0c8469d90d0abd9599f4595daf
Mateus Paixão <mateus.paixao@corebiz.com.br>
2020-07-06 14:43:29 -0300

    remove db dev from git, and add origin on cors

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const routes = require('./routes')
 const cors = require('cors')
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json())
 app.use(routes)
 
